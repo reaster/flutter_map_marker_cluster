aa095b686ddd5e57c259e1964b3490848209eed4
Richard Easterling <richard@Richards-MacBook-Pro.local>
2019-12-09 17:28:18 -0800

    marker_cluster_data_node.dart export

--- lib/flutter_map_marker_cluster.dart
+++ lib/flutter_map_marker_cluster.dart
@@ -3,3 +3,4 @@ library flutter_map_marker_cluster;
 export 'src/marker_cluster_layer.dart';
 export 'src/marker_cluster_layer_options.dart';
 export 'src/marker_cluster_plugin.dart';
+export 'src/marker_cluster_data_node.dart';
